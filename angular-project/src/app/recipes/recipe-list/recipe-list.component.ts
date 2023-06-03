c5223ed44d41190d5b681eed074bd09743d73cae
junkevin88 <junkevin88@gmail.com>
2023-06-03 09:02:57 +0700

    init project

---
+++ angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[]= [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
